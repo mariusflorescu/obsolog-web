dd49750a23b3039004ed2903b087d138b4dcafdd
Marius Florescu <mariusflorescu23@icloud.com>
2023-05-13 14:45:20 +0300

    chore(overview): add alert if no projects/channels

--- src/components/overview/content.tsx
+++ src/components/overview/content.tsx
@@ -1,14 +1,31 @@
 "use client";
 
+import { AlertOctagon } from "lucide-react";
 import { trpc } from "~/lib/trpc/client";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Spinner } from "../ui/spinner";
-import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
 
 export function Content() {
   const { data, isFetching } = trpc.overview.get.useQuery();
 
   if (isFetching) {
     return <Spinner />;
+  }
+
+  if (data?.numOfProjects === 0 && data?.numOfChannels === 0) {
+    return (
+      <Alert>
+        <AlertOctagon className="h-5 w-5" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          It looks like you haven't created any projects or channels yet.
+          <br />
+          In order to view overview insights, you need to have those and then
+          start sending events with <b>Obsolog</b>!
+        </AlertDescription>
+      </Alert>
+    );
   }
 
   return (
