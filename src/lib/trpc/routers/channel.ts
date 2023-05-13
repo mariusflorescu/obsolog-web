1ab1493dd6841507a2fe5ecd09f21b291040e7eb
Marius Florescu <mariusflorescu23@icloud.com>
2023-05-13 13:55:23 +0300

    feat(channel): add get procedure

--- src/lib/trpc/routers/channel.ts
+++ src/lib/trpc/routers/channel.ts
@@ -1,3 +1,20 @@
-import { t } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import { auth, t } from "../trpc";
+import { db } from "~/lib/db";
 
-export const channelRouter = t.router({});
+export const channelRouter = t.router({
+  get: t.procedure.use(auth).query(async ({ ctx }) => {
+    if (!ctx.tenant?.id) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Project actions require a tenant",
+      });
+    }
+
+    return db.channel.findMany({
+      where: {
+        tenantId: ctx.tenant.id,
+      },
+    });
+  }),
+});
