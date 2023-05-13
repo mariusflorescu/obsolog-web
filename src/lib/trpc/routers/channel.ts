b8279bc972bdbbc0991d628d99be7a4c7e88885c
Marius Florescu <mariusflorescu23@icloud.com>
2023-05-13 13:53:54 +0300

    feat: add channel router

---
+++ src/lib/trpc/routers/channel.ts
@@ -0,0 +1,3 @@
+import { t } from "../trpc";
+
+export const channelRouter = t.router({});
--- src/lib/trpc/routers/index.ts
+++ src/lib/trpc/routers/index.ts
@@ -1,8 +1,10 @@
 import { t } from "../trpc";
+import { channelRouter } from "./channel";
 import { projectRouter } from "./project";
 
 export const router = t.router({
   project: projectRouter,
+  channel: channelRouter,
 });
 
 export type Router = typeof router;
