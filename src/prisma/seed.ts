fcc9858db2bd6c91d0aa6d0ca3b6d3fdd1d44f94
Marius Florescu <mariusflorescu23@icloud.com>
2023-05-26 16:54:44 +0300

    chore: ignore seed command

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -7,20 +7,20 @@ const CHANNEL_ID = "cli378pev0001vl56rhr7bd41";
 const EVENT_NAME = "User Created";
 
 async function main() {
-  for (let i = 0; i < 600; ++i) {
-    const date = faker.date.between({ from: "2023-04-01", to: "2023-05-31" });
-    const id = uuid();
+  // for (let i = 0; i < 600; ++i) {
+  //   const date = faker.date.between({ from: "2023-04-01", to: "2023-05-31" });
+  //   const id = uuid();
 
-    await db.event.create({
-      data: {
-        name: EVENT_NAME,
-        user: id,
-        channelId: CHANNEL_ID,
-        tenantId: TENANT_ID,
-        createdAt: date,
-      },
-    });
-  }
+  //   await db.event.create({
+  //     data: {
+  //       name: EVENT_NAME,
+  //       user: id,
+  //       channelId: CHANNEL_ID,
+  //       tenantId: TENANT_ID,
+  //       createdAt: date,
+  //     },
+  //   });
+  // }
 }
 
 main()
