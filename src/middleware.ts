5d52f25c230df869b7e2b35f23a844364dc24f81
Marius Florescu <mariusflorescu23@icloud.com>
2023-05-29 16:22:07 +0300

    fix: try to fix CORS issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,14 @@ import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export default withClerkMiddleware((_req: NextRequest) => {
-  return NextResponse.next();
+export default withClerkMiddleware((req: NextRequest) => {
+  const response = NextResponse.next();
+
+  if (req.nextUrl.pathname.startsWith("/api/ingest")) {
+    response.headers.append("Access-Control-Allow-Origin", "*");
+  }
+
+  return response;
 });
 
 export const config = {
